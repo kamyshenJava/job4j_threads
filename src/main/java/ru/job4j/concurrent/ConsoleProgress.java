326ddf3ddfb413657c0832bec91ab2a92a75d574
kamyshenJava <kamyshen@tutanota.com>
2022-05-02 08:26:32 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        int flag = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                String symbol = (flag % 2) == 0 ? "/" : "\\";
+                System.out.print("\r load: " + symbol);
+                flag++;
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+}
