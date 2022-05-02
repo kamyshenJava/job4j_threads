9779c32309f33595a0976a1c9fcbb5af0090c73f
kamyshenJava <kamyshen@tutanota.com>
2022-05-02 03:25:28 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,17 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread first = new Thread(() -> {
+            for (int i = 0; i <= 100; i++) {
+                try {
+                    System.out.print("\rLoading : " + i + "%");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        first.start();
+    }
+}
