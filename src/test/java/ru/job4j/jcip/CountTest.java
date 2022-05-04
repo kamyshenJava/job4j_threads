c14daaebeec420f1b4b31d51c987428475a13a46
kamyshenJava <kamyshen@tutanota.com>
2022-05-04 09:47:08 +0300

    2. JCIP. Настройка библиотеки [#268575]

--- src/test/java/ru/job4j/jcip/CountTest.java
+++ src/test/java/ru/job4j/jcip/CountTest.java
@@ -5,9 +5,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CountTest {
-    /**
-     * Класс описывает нить со счетчиком.
-     */
+
     private class ThreadCount extends Thread {
         private final Count count;
 
@@ -23,18 +21,13 @@ public class CountTest {
 
     @Test
     public void whenExecute2ThreadThen2() throws InterruptedException {
-        /* Создаем счетчик. */
         final Count count = new Count();
-        /* Создаем нити. */
         Thread first = new ThreadCount(count);
         Thread second = new ThreadCount(count);
-        /* Запускаем нити. */
         first.start();
         second.start();
-        /* Заставляем главную нить дождаться выполнения наших нитей. */
         first.join();
         second.join();
-        /* Проверяем результат. */
         assertThat(count.get(), is(2));
 
     }
