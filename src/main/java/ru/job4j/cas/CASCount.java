7bddd9a7705d8e8fa2355cfbb05bb4faa050f3f0
kamyshenJava <kamyshen@tutanota.com>
2022-05-08 06:18:55 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -0,0 +1,23 @@
+package ru.job4j.cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>(0);
+
+    public void increment() {
+        int temp;
+        int newValue;
+        do {
+            temp = count.get();
+            newValue = temp + 1;
+        } while (!count.compareAndSet(temp, newValue));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
