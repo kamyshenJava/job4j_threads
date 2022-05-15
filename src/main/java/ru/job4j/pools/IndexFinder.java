9549c2dbe8c9ebb913479e4189960f1d32f042cd
kamyshenJava <kamyshen@tutanota.com>
2022-05-15 09:15:47 +0300

    3. ForkJoinPool [#315067 #300423]

--- src/main/java/ru/job4j/pools/IndexFinder.java
+++ src/main/java/ru/job4j/pools/IndexFinder.java
@@ -1,50 +1,50 @@
 package ru.job4j.pools;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class IndexFinder extends RecursiveTask<List<Integer>> {
+public class IndexFinder extends RecursiveTask<Integer> {
 
     private final Object[] array;
     private final Object target;
     private int from;
+    private int to;
 
     public IndexFinder(Object[] array, Object target) {
         this.array = array;
         this.target = target;
     }
 
-    public IndexFinder(Object[] array, Object target, int from) {
+    public IndexFinder(Object[] array, Object target, int from, int to) {
         this.array = array;
         this.target = target;
         this.from = from;
+        this.to = to;
     }
 
     @Override
-    protected List<Integer> compute() {
-        List<Integer> rsl = new ArrayList<>();
-        if (array.length < 11) {
-            for (int i = 0; i < array.length; i++) {
+    protected Integer compute() {
+        Integer rsl = null;
+        to = to == 0 ? array.length - 1 : to;
+        if (to - from < 11) {
+            for (int i = from; i <= to; i++) {
                 if (target.equals(array[i])) {
-                    rsl.add(i + from);
+                    rsl = i;
+                    break;
                 }
             }
         } else {
             IndexFinder left =
-                    new IndexFinder(Arrays.copyOfRange(array, 0, array.length / 2), target, from);
+                    new IndexFinder(array, target, from, (to - from) / 2 + from);
             IndexFinder right =
-                    new IndexFinder(Arrays.copyOfRange(array, array.length / 2,
-                            array.length), target,  from + array.length / 2);
-            rsl.addAll(left.invoke());
-            rsl.addAll(right.invoke());
+                    new IndexFinder(array, target,  from + (to - from) / 2 + 1, to);
+            Integer l = left.invoke();
+            rsl = l == null ? right.invoke() : l;
         }
         return rsl;
     }
 
-    public static List<Integer> find(Object[] array, Object target) {
+    public static Integer find(Object[] array, Object target) {
         ForkJoinPool pool = new ForkJoinPool();
         IndexFinder indexFinder = new IndexFinder(array, target);
         return pool.invoke(indexFinder);
---
+++ src/test/java/ru/job4j/pools/IndexFinderTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.pools;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.IntSupplier;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class IndexFinderTest {
+
+    private static class Obj {
+        int index;
+        String name;
+
+        public Obj(int index, String name) {
+            this.index = index;
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Obj obj = (Obj) o;
+            return index == obj.index;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(index);
+        }
+    }
+
+    @Test
+    public void findIndexThen31() {
+        List<Obj> temp = new ArrayList<>();
+        Obj target = new Obj(31, "b");
+        IntStream.range(0, 50).forEach(x -> temp.add(new Obj(x, "a")));
+        Obj[] arr = temp.toArray(new Obj[0]);
+        IndexFinder indexFinder = new IndexFinder(arr, target);
+        int expected = 31;
+        assertThat(expected, is(IndexFinder.find(arr, target)));
+    }
+
+    @Test
+    public void findIndexThen17() {
+        List<Obj> temp = new ArrayList<>();
+        Obj target = new Obj(17, "b");
+        IntStream.range(0, 50).forEach(x -> temp.add(new Obj(x, "a")));
+        Obj[] arr = temp.toArray(new Obj[0]);
+        IndexFinder indexFinder = new IndexFinder(arr, target);
+        int expected = 17;
+        assertThat(expected, is(IndexFinder.find(arr, target)));
+    }
+}
